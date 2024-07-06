3bd39e3ffdc628c208240009d2d1fd254b885f84
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-07-06 16:12:01 +0300

    Create valid password in test

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -3,24 +3,23 @@ from django.test import TestCase
 from taxi.forms import DriverCreationForm, DriverLicenseUpdateForm
 
 
-class FormsTest(TestCase):
-    def test_driver_creation_form(self):
-        form_data = {
+class FormTest(TestCase):
+    def test_driver_creation_form_with_license_number(self):
+        form_date = {
             "username": "TestDriver",
             "first_name": "TestFirst",
             "last_name": "TestLast",
-            "password1": "Test123",
-            "password2": "Test123",
+            "password1": "Test123.",
+            "password2": "Test123.",
             "license_number": "ABC12345",
         }
-        form = DriverCreationForm(data=form_data)
+        form = DriverCreationForm(data=form_date)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data, form_data)
+        self.assertEqual(form.cleaned_data, form_date)
 
-    def test_driver_license_update_form(self):
+    def test_driver_licence_update(self):
         form_data = {
-            "license_number": "ABC12345",
+            "license_number": "TES12345",
         }
         form = DriverLicenseUpdateForm(data=form_data)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data, form_data)
